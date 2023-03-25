093add9098c4a67786e122b6210c79a9760e59f5
Yetixxx <2272970495@qq.com>
2023-03-25 17:07:23 +0800

    完成断点续传

--- src/test/test.vue
+++ src/test/test.vue
@@ -2,6 +2,7 @@
     <div>
         <input accept=".zip" id="file" ref="testInput" type="file" name="" value="">
         <div @click="gotoServer">上传</div>
+        <div @click="abordtoServer">暂停</div>
     </div>
 </template>
 
@@ -16,10 +17,31 @@ onMounted(() => {
         submitUpload(UPLOAD_URL, testInput.value.files[0])
     })
 })
+const CancelToken = axios.CancelToken;
+let cancel = ref([]);
+let flag = ref(true)
+let TOKEN = null
+
+// 暂停上传 本质其实是中断Axios的请求
+const abordtoServer = () => {
+    try {
+        flag.value = false
+        localStorage.setItem('time', TOKEN)
+        cancel.value.forEach((val) => {
+            val();
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 
 async function submitUpload(url, file) {
-    const CHUNKSIZE = 5 * 1024 * 1024; // 1M
-    const TOKEN = Date.now();
+    const CHUNKSIZE = 15 * 1024 * 1024; // 5M
+    // TOKEN = Date.now();
+    TOKEN = localStorage.getItem('time')
+    console.log(TOKEN);
     //切割数组
     const chunkList = sliceFile(file, CHUNKSIZE);
     //创建formdata 并上传
@@ -31,16 +53,19 @@ async function submitUpload(url, file) {
     // if (sendChunkCount == chunkList.length) {
     let mergeFormData = new FormData();
     mergeFormData.append("type", "merge");
-    mergeFormData.append("token", TOKEN);
+    mergeFormData.append("token", localStorage.getItem('time'));
+    // mergeFormData.append("token", TOKEN);
     mergeFormData.append("chunkCount", chunkList.length);
     mergeFormData.append("fileName", file.name);
-    //结束后发送合并请
-    let res = await axios.post(url, mergeFormData);
-    console.log(res);
-    // }
+
+    if ((Object.keys((JSON.parse(localStorage.getItem('saveChunkKey'))))).length === 45) {
+        //结束后发送合并请
+        const res = await axios.post(url, mergeFormData)
+        console.log(res);
+    }
+
 
 }
-
 
 
 function createLimitPromise(limitNum, promiseListRaw) {
@@ -112,7 +137,6 @@ function getUploadedFromStorage() {
 //写入缓存
 function setUploadedToStorage(index) {
     let obj = getUploadedFromStorage()    //获取一下缓存
-    console.log(obj);
     obj[index] = true
     localStorage.setItem('saveChunkKey', JSON.stringify(obj))
 }
@@ -124,10 +148,8 @@ function createChunkPromiseList(chunkList, name, TOKEN) {
             console.log('index', index, uploadedInfo[index] ? '已上传过' : '未上传');
             if (uploadedInfo[index]) {//对比分段
                 sendChunkCount += 1;//记录已上传的索引
-                console.log(sendChunkCount);
-                console.log(chunkList.length);
+
             } else {
-                setUploadedToStorage(index)
                 let formdata = new FormData();
                 formdata.append("type", "upload");
                 formdata.append("name", name);
@@ -137,8 +159,13 @@ function createChunkPromiseList(chunkList, name, TOKEN) {
                 return formdata;
             }
         })
-        .map(formdata => {
-            return axios.post(UPLOAD_URL, formdata, axiosConfig);
+        .map((formdata, index) => {
+            return axios.post(UPLOAD_URL, formdata, axiosConfig).then(res => {
+                setUploadedToStorage(index)
+            }).catch(err => {
+                console.log(err);
+
+            });
         });
 }
 
@@ -150,8 +177,13 @@ const axiosConfig = {
     onUploadProgress: progressEvent => {
         const curPercent = (loadedLen / fileChunkLen * 100).toFixed(2)
         console.log(` percentage :${curPercent}%`)
-    }
+    },
+    cancelToken: new CancelToken(function executor(c) {
+        cancel.value.push(c);
+    }),
 };
+
+
 
 
 
